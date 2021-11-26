73a9d7979cccac9cfb3f36f676fbda0d40a5c744
iuliki <iulia.bulugu@emanuel.ro>
2021-11-26 08:14:20 +0200

    start

---
+++ vectoriinclasa/vectoriinclasa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vectoriinclasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
